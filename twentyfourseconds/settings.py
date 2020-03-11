4cdd861f8f9eb6b4f0b470dd0ce988a5d2d8df07
Jordan Morris <21jordanmorris@gmail.com>
2020-03-10 23:58:36 -0500

    Add whitenoise module for heroku

--- twentyfourseconds/settings.py
+++ twentyfourseconds/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'twentyfourseconds.urls'
@@ -124,10 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILE_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
