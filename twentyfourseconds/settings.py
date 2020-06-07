d91dc721022c830c99546159a495e4a3dab0f78a
Jordan Morris <21jordanmorris@gmail.com>
2020-06-07 11:08:25 -0500

    twentyfourseconds -> theFans; frontend only

--- twentyfourseconds/settings.py
+++ twentyfourseconds/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '^bk&o%t+p^z6@n3%#n7e_z5u6+k3^1xx)g&7f9%va7emb+7%ew'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'twentyfourseconds.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'thefans.herokuapp.com']
 
 
 # Application definition
