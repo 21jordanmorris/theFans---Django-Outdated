69eee8112edf6e4352ad482d5239bbc0b8cf7f1a
Jordan Morris <21jordanmorris@gmail.com>
2020-06-28 15:58:32 -0500

    Change home page objects for new schedule

--- pages/views.py
+++ pages/views.py
@@ -6,7 +6,9 @@ GameModel = apps.get_model('nba_games', 'Game')
 
 def index(request):
     post_queryset = PostModel.objects.all()[:3]
-    game_queryset = GameModel.objects.all()[:3]
+    count = GameModel.objects.count() - 88
+
+    game_queryset = GameModel.objects.all()[count:count+3]
 
     context = {
         "post_list": post_queryset,
